3664023b1751a957376bd4c9fe8bfd78cab7881a
ksuslov <ksuslov@azbukavkusa.ru>
2020-09-10 21:43:49 +0300

    ...

---
+++ GeekBrains_lesson_1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GeekBrains_lesson_1
+//
+//  Created by Константин Суслов on 07.09.2020.
+//  Copyright © 2020 Константин Суслов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+ 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()       
+    }
+
+
+   
+    
+    
+}
+
