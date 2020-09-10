a3d95fac52932a76f5d752b8a929cdfe85113523
ksuslov <ksuslov@azbukavkusa.ru>
2020-09-10 21:41:35 +0300

    first commit

---
+++ GeekBrains_lesson_1Tests/HomeWork.swift
@@ -0,0 +1,9 @@
+//
+//  HomeWork.swift
+//  GeekBrains_lesson_1Tests
+//
+//  Created by Константин Суслов on 10.09.2020.
+//  Copyright © 2020 Константин Суслов. All rights reserved.
+//
+
+import Foundation
