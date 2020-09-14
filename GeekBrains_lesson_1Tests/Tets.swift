c6a541cd184a7a31e369bf0004986e6f5a1b4bc5
ksuslov <ksuslov@azbukavkusa.ru>
2020-09-14 17:34:36 +0300

    homework 1

---
+++ GeekBrains_lesson_1Tests/Tets.swift
@@ -0,0 +1,9 @@
+//
+//  Tets.swift
+//  GeekBrains_lesson_1Tests
+//
+//  Created by Константин Суслов on 14.09.2020.
+//  Copyright © 2020 Константин Суслов. All rights reserved.
+//
+
+import Foundation
